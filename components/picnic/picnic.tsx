bd5cbb311d499b9d434ff31ebd3ffc35985020e4
Jerry Ochieng <jerryochieng178@gmail.com>
2024-11-18 18:49:29 +0300

    init

--- components/picnic/picnic.tsx
+++ components/picnic/picnic.tsx
@@ -4,6 +4,7 @@ import {Card, CardFooter, Image, Button,Link} from "@nextui-org/react";
 import picnic from "@/assets/niskize match day.jpg"
 import { siteConfig } from "@/config/site";
 import AOS from "aos";
+import {SportsSoccer} from "@mui/icons-material";
 
 export default function Picnic() {
     useEffect(() => {
@@ -26,19 +27,22 @@ export default function Picnic() {
 
             >
                 <Image
-                    alt="Picnic Poster"
+                    alt="Poster"
                     className="object-cover object-center"
                     height={"100%"}
                     src={picnic.src}
                     width={"100%"}
                 />
-              <CardFooter className="justify-between bg-blue-200 before:bg-pink-600/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-                    <p className="text-medium text-blue/80">Available Today.</p>
+              <CardFooter className="justify-between  before:bg-pink-600/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl
+              rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10" style={{height:"50px",background:"rgba(177,177,177,0.59)"}}>
+                    <p className="text-large text-blue/80">Available Today.</p>
                     <Button as={Link} href={siteConfig.links.sponsor}
                             className="text-tiny text-white bg-blue"
                             variant="shadow"
-                            color="secondary" radius="lg" size="sm">
-                        Buy Ticket
+                            color="secondary" radius="lg" size="lg"
+                            style={{background: "rgba(255,255,255,0.47)"}}
+                    >
+                        <SportsSoccer/> Buy Ticket
                     </Button>
                 </CardFooter>
             </Card>
